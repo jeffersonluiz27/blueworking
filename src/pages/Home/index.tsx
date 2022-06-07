a38d5391a0886ea04b80dcbd0231b371aed856a0
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-06 21:05:04 -0300

    iniciando repositorio e arquivos iniciais

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+function Home() {
+	return (
+		<>
+			<p>Cadastre espaços para devs e encontre grandes profissionais</p>
+			<form>
+				<label htmlFor="">E-mail</label>
+				<input type="text" name="" id="" />
+			</form>
+		</>
+	);
+}
+
+export default Home;
